eee8c1aebb35a165b99a36b92b2d592586edb6ff
prateek <prateekautomationlabs@gmail.com>
2025-12-28 11:39:35 +0530

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
       slowMo:1000
     },
      testIdAttribute: 'data-test',
-     storageState:'auth.json'
+     storageState: process.env.CI ? undefined : 'auth.json'
   },
 
   /* Configure projects for major browsers */
