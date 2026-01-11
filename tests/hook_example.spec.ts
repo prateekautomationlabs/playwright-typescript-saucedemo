e9d3154fcbc17d7b141df65ad546ff5ae7fc695d
prateek <prateekautomationlabs@gmail.com>
2026-01-11 10:12:34 +0530

    test

---
+++ tests/hook_example.spec.ts
@@ -0,0 +1,26 @@
+import {test} from '@playwright/test'
+
+test.beforeAll(async()=>{
+    console.log("Setting up things before all tests...")
+});
+
+test.beforeEach(async()=>{
+    console.log("Running before each test...")
+});
+
+test('Test1',()=>{
+    console.log('Running Test 1')
+});
+
+test('Test2',()=>{
+    console.log('Running Test 2')
+});
+
+test.afterEach(async()=>{
+    console.log("Test finished afterEach executed...")
+});
+
+
+test.afterAll(async()=>{
+    console.log("Cleaning up after all tests...")
+});
